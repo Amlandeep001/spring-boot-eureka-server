bb56ba8fe61e9dc6c96cd1e4f32245628841785b
Amlandeep001 <99658242+Amlandeep001@users.noreply.github.com>
2022-04-26 02:55:23 +0530

    Eureka Server related
    This is only for Eureka server

---
+++ src/main/java/com/hcl/product/server/ProductServerApplication.java
@@ -0,0 +1,20 @@
+package com.hcl.product.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 
+ * @author amlandeep.nandi
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ProductServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServerApplication.class, args);
+	}
+
+}
