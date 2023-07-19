6537ef06c7543e3aec2df592406ee7e63eb885f1
Amlan Deep Nandi <99658242+Amlandeep001@users.noreply.github.com>
2023-07-20 03:09:09 +0530

    Java and Spring Boot version upgrades
    Java and Spring Boot version upgrades
    Java 17
    Spring Boot 3.1.1
    Spring cloud 2022.0.3

--- src/main/java/com/abc/product/server/ProductServerApplication.java
+++ src/main/java/com/abc/product/server/ProductServerApplication.java
@@ -11,9 +11,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class ProductServerApplication {
+public class ProductServerApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(ProductServerApplication.class, args);
 	}
 
